c9988471eab3bd5c0aa1484a5b56fcd9ff9435c5
Sulaksa Jeevakumar <jeevakus@mcmaster.ca>
2021-02-20 11:26:58 -0500

    updated send_sms.py with message parameter & deleted testing call

--- send_sms.py
+++ send_sms.py
@@ -2,16 +2,13 @@
 from twilio.rest import Client
 
 # the following line uses your Twilio Account SID and Auth Token
-client = Client("ACbe61ee2181e6cbe1334a7885c363dca8","afe4103687a7691025db12e10012335d")
+client = Client("ACbe61ee2181e6cbe1334a7885c363dca8","335e51fcc19d8a6b69ceebade07ad969")
 
 # "from_" number is your Twilio number and the "to" number
 # is the phone number you signed up for Twilio with, or upgrade your 
 # account to send SMS to any phone number
-
 def call(capacity, spots,message):
 	sms = client.messages.create(to="+16476252949", 
 						   from_ ="+15717770102",
 						   body=message)  
 	return sms
-
-call(20,0,"Hello")
