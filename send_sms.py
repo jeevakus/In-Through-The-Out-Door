50770dd9540849bb830eee006318e9333e389559
Sulaksa <jeevakus@mcmaster.ca>
2021-02-20 11:42:29 -0500

    added arduino python serial comm file, with function sms call from send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -7,7 +7,7 @@ client = Client("ACbe61ee2181e6cbe1334a7885c363dca8","335e51fcc19d8a6b69ceebade0
 # "from_" number is your Twilio number and the "to" number
 # is the phone number you signed up for Twilio with, or upgrade your 
 # account to send SMS to any phone number
-def call(capacity, spots,message):
+def call(message):
 	sms = client.messages.create(to="+16476252949", 
 						   from_ ="+15717770102",
 						   body=message)  
